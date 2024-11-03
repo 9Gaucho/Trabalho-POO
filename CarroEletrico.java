b0ff00f463778a5fe33e72e1deafccef13d4bfd2
Samuel Watthier <98723071+9Gaucho@users.noreply.github.com>
2024-11-03 15:15:05 -0300

    Update CarroEletrico.java

--- CarroEletrico.java
+++ CarroEletrico.java
@@ -1,1 +1,41 @@
-aaa
+public class CarroEletrico {
+    protected String id;
+    protected String marca;
+    protected String modelo;
+    protected int ano;
+    protected double bateria;
+    protected double autonomiaAtual;
+
+    public CarroEletrico(String id, String marca, String modelo, int ano, double bateria, double autonomiaAtual) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.bateria = bateria;
+        this.autonomiaAtual = autonomiaAtual;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public double getBateria() {
+        return bateria;
+    }
+
+    public double getAutonomiaAtual() {
+        return autonomiaAtual;
+    }
+}
